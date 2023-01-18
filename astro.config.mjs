3605e7ebc15d2e1a2826e14180a85d7ca883ebc7
Geoffrey Hunter <geoff@biolumic.com>
2023-01-18 17:29:55 +1300

    Adding image support.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,14 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 
 // https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
   // Enable Preact to support Preact JSX components.
   preact(),
   // Enable React for the Algolia search component.
-  react(), mdx()],
+  react(), mdx(), image()],
   site: `https://astro.build`
 });
